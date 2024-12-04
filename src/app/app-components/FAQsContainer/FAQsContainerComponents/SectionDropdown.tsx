7c3b59fdf45a23e0b2ae47d29187117903e0d6e2
titaninnovation-it <admin@titaninnovation.com.my>
2024-12-04 18:39:17 +0800

    Added FAQ

--- src/app/app-components/FAQsContainer/FAQsContainerComponents/SectionDropdown.tsx
+++ src/app/app-components/FAQsContainer/FAQsContainerComponents/SectionDropdown.tsx
@@ -15,24 +15,31 @@ export default function SectionDropdown() {
         "Titan Innovation is Malaysia's leading trading platform for construction and heavy equipment. Fully digital with everything under one platform, we provide you with an easy, trustworthy, and cost- efficient way to buy, sell, or rent heavy construction equipment. We also provide after sales services such as insurance for your machinery.",
     },
     {
-      title: "Why choose us?",
-      description: "",
+      title: "How do I get in touch with Titan Innovation?",
+      description: `You can reach us through the following channels:\n
+
+        Telephone:\n
+          (+60) 03 – 8051 0988\n
+          (+60) 19 – 355 7588\n
+        Facebook:\n
+        E-mail us:  admin@titaninnovation.com.my\n
+        Visit our website: titaninnovation.com.my`,
     },
     {
       title: "What are your operation hours?",
       description: `Inspection service: \nMonday – Sat (9:00am – 6:00pm).\n\nCustomer service:\n Monday – Sunday (9:00am – 6:00pm).`,
     },
     {
-      title: "Why should i buy from Titan Innovation?",
-      description: "",
+      title: "Why should I buy from Titan Innovation?",
+      description: "All machinery on Titan Innovation are fully inspected and verified by professional engineers to determine its realistic value. We ensure only the highest of quality and safety are available for you. ",
     },
     {
       title: "What is full inspection report?",
-      description: "",
+      description: "A full inspection report is a procedure that our professional engineers will take to check the components of your machine, from top to bottom.",
     },
     {
-      title: "Why should i sell with Titan Innovation?",
-      description: "",
+      title: "Why should I sell with Titan Innovation?",
+      description: "You have the machinery and we have the expertise. By selling with Titan Innovation, you can leverage on our years of experience and market knowledge to understand the working conditions and internal /external condition of your machinery. ",
     },
   ];
   
