5afd24fdc397e19cd0e71a11afd23a76d4b9ec4a
dylantee <aksoonz@gmail.com>
2024-12-14 11:26:25 +0800

    tweaked header and footer ui

--- src/components/Footer/CompanyInfo.tsx
+++ src/components/Footer/CompanyInfo.tsx
@@ -1,32 +1,56 @@
 import useIsMobile from "@/libs/useIsMobile";
 import Text from "../Shared/Text";
+import Image from "next/image";
+import CustomIcon from "../Shared/CustomIcon";
 
 export default function CompanyInfo() {
   const isMobile = useIsMobile();
   return (
     <div className={`flex flex-1 flex-col mb-10`}>
-      <img
-        src={`/titan_innovation_logo.png`}
-        alt=""
-        className={`${isMobile ? `mx-8` : ``}`}
+      <div
+        className={`flex mb-4 ${isMobile ? `self-center` : ``}`}
         style={{
-          width: 125,
-          height: 50,
-          objectFit: "contain",
-          marginBottom:10
+          position: "relative",
+          height: isMobile ? 60 : 60,
+          width: isMobile ? 140 : 120,
         }}
-      />
+      >
+        <Image
+          src={`/titan_innovation_logo.png`}
+          alt=""
+          fill
+          objectFit="contain"
+        />
+      </div>
       <Text
         title="Buy, Sell or Rent your machinery with Ease"
         size={`small`}
-        className="mb-4"
+        className="mb-4 text-[#5E6282]"
       />
-      <Text
-        title="No. 34-2, Jalan Puteri 4/8, Bandar Puteri Puchong 47100 Puchong,Selangor"
-        size={`small`}
-        className="mb-2"
-      />
-      <Text title="+6019-3557588" size={`small`} className="mb-2" />
+      <div className="flex gap-2 items-start justify-start mb-4">
+        <CustomIcon
+          name="heroicons:map-pin-20-solid"
+          size={isMobile ? 25 : 45}
+          color={"#000000"}
+        />
+        <Text
+          title="No. 34-2, Jalan Puteri 4/8, Bandar Puteri Puchong 47100 Puchong,Selangor"
+          size={`small`}
+          className="text-[#5E6282]"
+        />
+      </div>
+      <div className="flex gap-2 items-start justify-start">
+        <CustomIcon
+          name="ic:round-phone"
+          size={isMobile ? 20 : 15}
+          color={"#000000"}
+        />
+        <Text
+          title="+6019-3557588"
+          size={`small`}
+          className="text-[#5E6282]"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Footer/SocialAndDownloadApp.tsx
+++ src/components/Footer/SocialAndDownloadApp.tsx
@@ -9,7 +9,7 @@ import useIsMobile from "@/libs/useIsMobile";
 export default function SocialAndDownloadApp() {
   const isMobile = useIsMobile();
   return (
-    <div className="flex flex-1 flex-col">
+    <div className={`flex flex-1 flex-col ${isMobile?``:`px-10`}`}>
       <div className="flex gap-4 mb-4">
         <ButtonIcon
           icon={
@@ -33,36 +33,47 @@ export default function SocialAndDownloadApp() {
           onClick={() => {}}
           className="p-2 rounded-full shadow"
         />
-        <ButtonIcon
-          icon={<CustomIcon name="mdi:twitter" size={25} color={"#000000"} />}
-          onClick={() => {}}
-          className="p-2 rounded-full shadow"
-        />
       </div>
       <Text title="Discover our app" size={"big"} />
       <div className="flex flex-row gap-2 mt-2">
-        <Image
+        <div
+          className="flex"
+          style={{
+            position: "relative",
+            height: isMobile ? 60 : 60,
+            width: isMobile ? 140 : 120,
+          }}
           onClick={() => {
             window.location.href =
               "https://apps.apple.com/us/app/titan-innovation/id6736380841";
           }}
-          src={`/download-on-the-appstore.png`}
-          alt=""
-          width={isMobile ? 80 : 120}
-          height={isMobile ? 80 : 120}
-          objectFit="contain"
-        />
-        <Image
+        >
+          <Image
+            src={`/download-on-the-appstore.png`}
+            alt=""
+            fill
+            objectFit="contain"
+          />
+        </div>
+        <div
+          className={`flex`}
+          style={{
+            position: "relative",
+            height: isMobile ? 60 : 60,
+            width: isMobile ? 140 : 120,
+          }}
           onClick={() => {
             window.location.href =
               "https://play.google.com/store/apps/details?id=com.titanhmapp";
           }}
-          src={`/get-it-on-google-play.png`}
-          alt=""
-          width={isMobile ? 80 : 120}
-          height={isMobile ? 80 : 120}
-          objectFit="contain"
-        />
+        >
+          <Image
+            src={`/get-it-on-google-play.png`}
+            alt=""
+            fill
+            objectFit="contain"
+          />
+        </div>
       </div>
     </div>
   );
